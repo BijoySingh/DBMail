2891bf7f7118a5af2e8a64bd50cd281f75a3a78c
Bijoy Singh <bijoysingh693@gmail.com>
2014-10-15 19:08:28 +0530

    Removing Metadata

--- DBMailProject/src/DBMailPackage/InboxHandler.java
+++ DBMailProject/src/DBMailPackage/InboxHandler.java
@@ -1,36 +1,81 @@
 package DBMailPackage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class InboxHandler
+ * Servlet implementation class PSQLConnector
  */
 public class InboxHandler extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	Connection conn1 =null;
+	Statement st =null;
+	String source_link = "/LibraryApplication/";
+	public void init() throws ServletException {
+		//Open the connection here
 
-    /**
-     * Default constructor. 
-     */
-    public InboxHandler() {
-        // TODO Auto-generated constructor stub
-    }
+		String dbURL2 = "jdbc:postgresql://10.105.1.12/cs387";
+		String user = "db120050006";
+		String pass = "120050006";
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			conn1 = DriverManager.getConnection(dbURL2, user, pass);
+			st = conn1.createStatement();
+			System.out.println("init"+conn1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public void destroy() {
+		//Close the connection here
+		try{
+			conn1.close();
+			System.out.println("close");
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	protected void getEmail(String userid, HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException{
+		
+		
+		JSONObject jo = new JSONObject();
+		jo.put("firstName", "John");
+		jo.put("lastName", "Doe");
+
+		JSONArray ja = new JSONArray();
+		ja.put(jo);
+
+		JSONObject mainObj = new JSONObject();
+		mainObj.put("employees", ja);		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException
+	{
+		 HttpSession session = request.getSession();
+	     String userid = request.getParameter( "user_id" );
+//		 String cc = request.getParameter("cc_receivers");
+//		 String bcc = request.getParameter("bcc_receivers");
+	     getEmail(userid,request,response);
+	     
+	     response.sendRedirect(source_link + "menu?issue=something");		
 	}
 
 }
